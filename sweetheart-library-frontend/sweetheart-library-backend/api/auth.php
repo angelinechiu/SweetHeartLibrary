a337821ae308f1911f2c09e641876e23461a3bb3
angelinec <angelinechiu52@gmail.com>
2026-06-03 16:54:54 +0800

    forgot password

--- sweetheart-library-frontend/sweetheart-library-backend/api/auth.php
+++ sweetheart-library-frontend/sweetheart-library-backend/api/auth.php
@@ -75,7 +75,6 @@ if ($action === 'forgot_password' && $method === 'POST') {
         require_once __DIR__ . '/../PHPMailer/src/PHPMailer.php';
         require_once __DIR__ . '/../PHPMailer/src/SMTP.php';
         require_once __DIR__ . '/../PHPMailer/src/Exception.php';
-
         try {
             $phpmailer->isSMTP();
             $phpmailer->Host       = 'sandbox.smtp.mailtrap.io';
